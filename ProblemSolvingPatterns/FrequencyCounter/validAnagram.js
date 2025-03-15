5d2e9d965908fe314cdb15b9b4e4f53c02030d3d
theTrackSuitDev <dimiro89@gmail.com>
2025-03-15 03:22:21 +0200

    Frequency counter

---
+++ ProblemSolvingPatterns/FrequencyCounter/validAnagram.js
@@ -0,0 +1,29 @@
+function validAnagram(strOne, strTwo) {
+    if (strOne.length !== strTwo.length) {
+        return false;
+    }
+
+    const frequencyObjOne = {};
+    for (const char of strOne) {
+        frequencyObjOne[char] = (frequencyObjOne[char] || 0) + 1;
+    }
+
+    const frequencyObjTwo = {};
+    for (const char of strTwo) {
+        frequencyObjTwo[char] = (frequencyObjTwo[char] || 0) + 1;
+    }
+
+    for (const char in frequencyObjOne) {
+        if (!Object.hasOwn(frequencyObjTwo, char)) {
+            return false;
+        }
+
+        if (frequencyObjOne[char] !== frequencyObjTwo[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(validAnagram("qwerty", "qeywrt"));
