7c315a37f9fdefa9a0206717dc2237128bb6a925
theTrackSuitDev <dimiro89@gmail.com>
2025-08-10 03:15:50 +0300

    BST Methods

--- DataStructures/BinarySearchTree/binarySearchTree.js
+++ DataStructures/BinarySearchTree/binarySearchTree.js
@@ -66,6 +66,126 @@ class BinarySearchTree {
             }
         }
     }
+
+    remove(value) {
+        if (this.root === null) {
+            return;
+        }
+
+        let removedNode = null;
+
+        function findAndRemove(node) {
+            if (value === node.value) {
+                removedNode = new Node(node.value);
+                if (node.left && node.right) {
+                    let current = node.right;
+                    while (current.left) {
+                        current = current.left;
+                    }
+
+                    node.value = current.value;
+                    current.value = value;
+                    node.right = findAndRemove(node.right);
+                } else if (node.left) {
+                    return node.left;
+                } else if (node.right) {
+                    return node.right;
+                } else {
+                    return null;
+                }
+            } else if (value < node.value) {
+                if (node.left) {
+                    node.left = findAndRemove(node.left);
+                }
+            } else {
+                if (node.right) {
+                    node.right = findAndRemove(node.right);
+                }
+            }
+            return node;
+        }
+        this.root = findAndRemove(this.root);
+        return removedNode;
+    }
+
+    depthFirstSearchOrdered() {
+        let result = [];
+
+        function traverse(node) {
+            if (node.left) {
+                traverse(node.left);
+            }
+
+            result.push(node.value);
+
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+
+        if (this.root === null) {
+            return null;
+        }
+
+        traverse(this.root);
+        return result;
+    }
+
+    findSecondLargest() {
+        let orderedNodes = this.depthFirstSearchOrdered();
+
+        if (orderedNodes === null) {
+            return undefined;
+        }
+
+        if (orderedNodes.length === 0) {
+            return 0;
+        }
+
+        if (orderedNodes.length === 1) {
+            return 0;
+        }
+
+        return orderedNodes[orderedNodes.length - 2];
+    }
+
+    isBalanced() {
+        if (this.root && !this.root.left && !this.root.right) {
+            return true;
+        }
+
+        let counter = 0;
+
+        let child = this.root.left;
+        if (this.root.left) {
+            while (child.left || child.right) {
+                if (child.left) {
+                    child = child.left;
+                } else if (child.right) {
+                    child = child.right;
+                }
+                counter++;
+            }
+        }
+
+        child = this.root.right;
+        if (this.root.right) {
+            while (child.left || child.right) {
+                if (child.left) {
+                    child = child.left;
+                } else if (child.right) {
+                    child = child.right;
+                }
+                counter--;
+            }
+        }
+
+        if (counter < 0 || counter > 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 let tree = new BinarySearchTree();
