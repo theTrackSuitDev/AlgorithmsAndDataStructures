667c907f43cd42b2faeac47c475309bdbe1c1c21
theTrackSuitDev <dimiro89@gmail.com>
2025-08-11 02:29:27 +0300

    Binary Heap

--- DataStructures/Heap/maxBinaryHeap.js
+++ DataStructures/Heap/maxBinaryHeap.js
@@ -16,9 +16,64 @@ class MaxBinaryHeap {
 
         return index;
     }
+
+    extractMax() {
+        let max = this.values[0];
+        this.values[0] = this.values[this.values.length - 1];
+        this.values.pop();
+        this.sinkDown();
+        return max;
+    }
+
+    sinkDown() {
+        let index = 0;
+        let parent = this.values[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild;
+            let rightChild;
+            let swap = null;
+
+            if (leftChildIndex < this.values.length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild > parent) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < this.values.length) {
+                rightChild = this.values[rightChildIndex];
+                if (
+                    (rightChild > parent && swap === null) || 
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) {
+                break;
+            }
+
+            this.values[index] = this.values[swap];
+            this.values[swap] = parent;
+            index = swap;
+        }
+    }
 }
 
 let maxHeap = new MaxBinaryHeap();
-maxHeap.values = [41,39,33,18,27,12];
+maxHeap.values = [41, 39, 33, 18, 27, 12];
 
 console.log(maxHeap.insert(55));
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+console.log(maxHeap.extractMax());
+console.log(maxHeap.values);
